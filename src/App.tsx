eaa8d1e9bce6d015fb58e7e29db7e8cbd01f65c9
OJ Abba <phavorabba@gmail.com>
2025-05-29 12:33:28 +0100

    [Chore] Introduce the `ErrorFallback` component

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,19 @@ import "simplebar/dist/simplebar.min.css";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import { Feedback } from "./pages/Feedback";
+import { ErrorBoundary } from "@sentry/react";
+import { ErrorFallback } from "./components/ErrorFallback/ErrorFallback";
 
 function App() {
   return (
     <DeskproAppProvider>
       <HashRouter>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route index path="/feedback/:id" element={<Feedback />}></Route>
-        </Routes>
+        <ErrorBoundary fallback={ErrorFallback}>
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route index path="/feedback/:id" element={<Feedback />}></Route>
+          </Routes>
+        </ErrorBoundary>
       </HashRouter>
     </DeskproAppProvider>
   );
